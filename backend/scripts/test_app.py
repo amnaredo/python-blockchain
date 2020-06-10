1d1d72195b9a3036008edb1de44391c9063dc43d
amnaredo <amnaredo@gmail.com>
2020-06-10 17:05:41 +0200

    Transaction chain validations

--- backend/scripts/test_app.py
+++ backend/scripts/test_app.py
@@ -24,6 +24,10 @@ def post_wallet_transact(recipient, amount):
     ).json()
 
 
+def get_wallet_info():
+    return requests.get(f'{BASE_URL}/wallet/info').json()
+
+
 start_blockchain = get_blockchain()
 
 print(f'start_blockchain: {start_blockchain}')
@@ -39,3 +43,6 @@ print(f'\npost_wallet_transact_2: {post_wallet_transact_2}')
 time.sleep(1)
 mined_block = get_blockchain_mine()
 print(f'\nmined_block: {mined_block}')
+
+wallet_info = get_wallet_info()
+print(f'\nwallet info: {wallet_info}')
